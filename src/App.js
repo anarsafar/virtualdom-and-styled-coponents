667c3f9f5427752cb704d381283408c9ba267cd4
Anar Safarov <anarsferov@gmail.com>
2022-12-16 21:09:41 +0400

    initial files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// import Style from "./style.module.css";
+
+// import "./index.css";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: red;
+  background-color: darkcyan;
+`;
+
+const Button = styled.button`
+  color: ${(prop) => (prop.primary ? "blue" : "gray")};
+  background: ${(prop) => (prop.primary ? "gold" : "black")};
+`;
+
+const FancyButton = styled(Button)`
+  border: 10px solid red;
+`;
+
+const users = [
+  { name: "Megan Fox" },
+  { name: "Megan Fox" },
+  { name: "Megan Fox" },
+  { name: "Megan Fox" },
+  { name: "Megan Fox" },
+  {},
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello React 2</h1>
+      <Title>Hello styled</Title>
+      <Button href="www." as={"a"} primary>
+        Click me
+      </Button>
+      <FancyButton primary>fancy</FancyButton>
+
+      {users.map((user, index) => (
+        <p key={index}>{user.name}</p>
+      ))}
     </div>
   );
 }
